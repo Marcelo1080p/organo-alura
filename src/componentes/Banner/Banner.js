7aeaa9987ece8dc5c822777931e01c26cda5af6b
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-05 20:02:29 -0300

    PROJETO ORGANO

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Banner = () => {
+    return (
+        <img src="https://caelum-online-public.s3.amazonaws.com/2535-react-com-javascript/Transcri%C3%A7%C3%A3o+/Aula+1/01.04_001_imagem7.png" alt="Banner principal"/>
+    )
+}
+
+export default Banner
