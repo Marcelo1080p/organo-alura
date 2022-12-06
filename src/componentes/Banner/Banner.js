805cf061026122355086135cfa3652d264474df9
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-05 21:20:56 -0300

    novos componentes criados

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,8 +1,11 @@
 import React from "react";
-
+import "./Banner.css"
 const Banner = () => {
     return (
-        <img src="https://caelum-online-public.s3.amazonaws.com/2535-react-com-javascript/Transcri%C3%A7%C3%A3o+/Aula+1/01.04_001_imagem7.png" alt="Banner principal"/>
+        <header className="banner">
+            <img src="https://caelum-online-public.s3.amazonaws.com/2535-react-com-javascript/Transcri%C3%A7%C3%A3o+/Aula+1/01.04_001_imagem7.png" alt="Banner principal"/>
+        </header>
+        
     )
 }
 
