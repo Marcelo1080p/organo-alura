a731bc5e1a127dae736b2e2db85531de626c81ee
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-06 20:00:19 -0300

    botao criado

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,16 +1,36 @@
 import React from "react";
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto"
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (e) => {
+      e.preventDefault()
+      console.log("Formulario salvo")
+    }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
         <CampoTexto nome="Nome" placeholder="Digite seu nome" />
         <CampoTexto nome="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto nome="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times}/>
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
