067345f2b478d3422a571d7b96a14724b3bed203
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-09 22:14:11 -0300

    adionando partes dos colaboradores

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import "./Colaborador.css"
+
+const Colaborador = () => {
+    return (<div className='colaborador'> 
+        <div className='cabecalho'>
+            <img src='https://github.com/marcelo1080p.png' alt=""/>
+        </div>
+        <div className='rodape'>
+            <h4>Marcelo Junior</h4>
+            <h5>Instrutor</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,13 @@
+import Colaborador from "../Colaborador"
 import "./Time.css"
 
 const Time = (props) => {
     return (
         <section style={{background: props.corSecundaria }} className="time">
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <Colaborador/>
+            <Colaborador/>
+
         </section>
     )
 }
